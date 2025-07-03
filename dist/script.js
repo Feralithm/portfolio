5d0e134ff79859053e30fc6d7d421a5105a0d3a7
Feralithm <frdytmvn26@gmail.com>
2025-07-03 17:22:18 +0800

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,39 @@
+// Hamburger
+const hb = document.querySelector('#hamburger');
+const navmenu = document.querySelector('#nav-menu');
+
+hb.addEventListener('click', function() {
+    hb.classList.toggle('hamburger-aktif')
+    navmenu.classList.toggle('hidden')
+});
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxGoPifMcR1cYFL4Xkssvizj6Mt59UUMCayrEvOu4QrSTZuZtgRbzfGr9eVITD63vSJ/exec'
+    const form = document.forms['My-web-portfolio contact form'];
+    const myAlert = document.getElementById('alert');
+    const btn = document.getElementById('btn');
+    
+    // Button load
+    btn.addEventListener('click', function() {
+      btn.innerHTML = '<i class="fa fa-spinner fa-spin"></i>Mengirim!'
+    });
+
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+          myAlert.classList.toggle('hidden');
+          myAlert.classList.toggle('succsess');
+          myAlert.innerHTML += 'Terima kasih, pesan anda sudah kami terima!';
+          btn.innerHTML = 'Kirim';
+          console.log('succsess!', response);
+          form.reset();
+        })
+        .catch(error => {
+          myAlert.classList.toggle('hidden');
+          myAlert.classList.toggle('danger');
+          myAlert.innerHTML += 'Terjadi kesalahan, gagal mengirim pesan⚠️';
+          btn.innerHTML = 'Kirim';
+          alert(error.message);
+          form.reset();
+        });
+    });
